386d90653e2fcc0c06b937168ec9b3a93480e344
Rasika Premachandra <130959620+rasikww@users.noreply.github.com>
2024-12-30 19:58:01 +0530

    added some comments for future implementations

--- src/components/event/shareBar.jsx
+++ src/components/event/shareBar.jsx
@@ -1,10 +1,14 @@
+"use client";
 import { Button } from "../ui/button";
 import { IoHandLeft } from "react-icons/io5";
 import ShareIcon from "@/components/icon/shareIcon";
 export default function ShareBar() {
+    //user should be added as a prop here, then it should be passed down
+    // to i'm in button to track down, due to unavailable data it is not added
     return (
         <div className="flex px-9 mt-8 gap-5">
             <Button
+                onClick={() => console.log("I'm coming to this event!")}
                 variant="outline"
                 className="w-full rounded-3xl py-6 text-lg bg-white border-mint-500 border-2 hover:text-mint-500"
             >
@@ -12,6 +16,7 @@ export default function ShareBar() {
                 <IoHandLeft style={{ width: "20px", height: "20px" }} />
             </Button>
             <Button
+                onClick={() => console.log("I'm sharing this event!")}
                 variant="outline"
                 className="w-2/5 lg:w-full rounded-3xl px-4 py-6 text-lg bg-white border-mint-500 border-2 hover:text-mint-500 shrink"
             >
--- src/components/ui/addComment.jsx
+++ src/components/ui/addComment.jsx
@@ -1,3 +1,4 @@
+"use client";
 import * as React from "react";
 import Image from "next/image";
 import { Input } from "./input";
@@ -6,6 +7,8 @@ import sendIcon from "@/assets/img/send-icon.png";
 
 export const CommentWithIcon = React.forwardRef(
     ({ className, iconSrc, alt, type, ...props }, ref) => {
+        //user should be added as a prop here, then it should be passed down
+        // to send button to track down, due to unavailable data it is not added
         return (
             <div className="relative flex">
                 <Input
@@ -19,6 +22,11 @@ export const CommentWithIcon = React.forwardRef(
                     ref={ref}
                 />
                 <button
+                    onClick={() => console.log("adding a comment...")}
+                    //send button should collect the user and comment then send it
+                    // to the backend and update the front end according to the new comments array
+                    //consider adding a loading state to load the new comments
+                    //consider adding enter button to send the comment
                     type="button"
                     className="absolute top-1/2 right-3 -translate-y-1/2 flex items-center justify-center w-8 h-8 rounded-full hover:bg-mint-600"
                 >
