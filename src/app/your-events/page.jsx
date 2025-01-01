83c03a922d7f986c560a4ce974dabfd55cb67b52
Rasika Premachandra <130959620+rasikww@users.noreply.github.com>
2025-01-01 10:34:50 +0530

    add your events cards

---
+++ src/app/your-events/page.jsx
@@ -0,0 +1,12 @@
+import YourEventsCard from "@/components/event/yourEventsCard";
+export default function YourEvents() {
+    return (
+        <>
+            <section>
+                {/* {"add your events card"} */}
+                <YourEventsCard />
+            </section>
+            <section>{/* {"add tabs component with events"} */}</section>
+        </>
+    );
+}
