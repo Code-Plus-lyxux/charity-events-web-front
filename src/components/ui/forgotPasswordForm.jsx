6dc207e2d8566874a06a95b1496b39a33b44d703
Rasika Premachandra <130959620+rasikww@users.noreply.github.com>
2025-01-03 11:45:11 +0530

    add forgot password page to add email to verify

--- src/components/ui/forgotPasswordForm.jsx
+++ src/components/ui/forgotPasswordForm.jsx
@@ -3,7 +3,6 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
-import Link from "next/link";
 import emailIcon from "@/assets/img/email-icon.png";
 import { InputWithIcon } from "@/components/ui/inputWithIcon";
 import { Button } from "@/components/ui/button";
@@ -14,28 +13,30 @@ import {
     FormItem,
     FormMessage,
 } from "@/components/ui/form";
-import { PasswordInput } from "@/components/ui/passwordInput";
+import { useAuth } from "@/hooks/authContext";
+import { useRouter } from "next/navigation";
 
 // Define the form schema
 const formSchema = z.object({
     email: z.string().email(),
-    password: z.string().min(6, {
-        message: "Password must be at least 6 characters.",
-    }),
 });
 
-export default function LoginForm() {
+export default function ForgotPasswordForm() {
+    const { setEmail } = useAuth();
+    const router = useRouter();
+
     // Initialize useForm
     const form = useForm({
         resolver: zodResolver(formSchema),
         defaultValues: {
             email: "",
-            password: "",
         },
     });
 
     // Handle form submission
     const onSubmit = (values) => {
+        setEmail(values.email);
+        router.push("/auth/verify");
         console.log("Form submitted with values:", values); //TODO: add changes here
     };
 
@@ -43,7 +44,7 @@ export default function LoginForm() {
         <Form {...form}>
             <form
                 onSubmit={form.handleSubmit(onSubmit)}
-                className="space-y-6 w-full max-w-sm"
+                className="space-y-14 w-full max-w-sm"
             >
                 <FormField
                     control={form.control}
@@ -64,34 +65,12 @@ export default function LoginForm() {
                         </FormItem>
                     )}
                 />
-                <FormField
-                    control={form.control}
-                    name="password"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormControl>
-                                <PasswordInput
-                                    className="pl-10 placeholder:text-gray-500 appearance-none"
-                                    {...field}
-                                />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <div className="flex justify-end">
-                    <Link
-                        href="/auth/forgot-password"
-                        className="text-sm font-bold text-mint-800 hover:text-mint-600  dark:text-mint-500 dark:hover:text-mint-300 "
-                    >
-                        Forgot password?
-                    </Link>
-                </div>
+
                 <Button
                     type="submit"
                     className="w-full rounded-3xl py-6 text-lg bg-mint-500 hover:bg-mint-700"
                 >
-                    LOGIN
+                    SUBMIT
                 </Button>
             </form>
         </Form>
