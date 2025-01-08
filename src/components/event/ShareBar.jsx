08fa6002d7fbbb50f7cc494e30cb229a263dcac1
Rasika Premachandra <130959620+rasikww@users.noreply.github.com>
2025-01-08 23:25:43 +0530

    add I'm In button api calling

--- src/components/event/ShareBar.jsx
+++ src/components/event/ShareBar.jsx
@@ -1,19 +1,91 @@
 "use client";
 import { Button } from "@/components/ui/Button";
 import { IoHandLeft } from "react-icons/io5";
+import { CircleX } from "lucide-react";
 import ShareIcon from "@/components/icon/ShareIcon";
+import { useState } from "react";
+import axios from "axios";
+import { useParams } from "next/navigation";
 export default function ShareBar() {
-    //user should be added as a prop here, then it should be passed down
-    // to i'm in button to track down, due to unavailable data it is not added
+    const [isGoing, setIsGoing] = useState(false);
+    const [isHovering, setIsHovering] = useState(false);
+    const { id } = useParams();
+
+    async function handleGoingToggle() {
+        try {
+            //prev state:isGoing false means user marked it from not attending to attending
+            //after clicking next state will be true
+            if (!isGoing) {
+                const response = await axios.post(
+                    `http://localhost:5001/api/events/${id}/attend`,
+                    {},
+                    {
+                        headers: {
+                            Authorization: `Bearer ${localStorage.getItem(
+                                "userToken"
+                            )}`,
+                        },
+                    }
+                );
+                if (response.status === 200) {
+                    setIsGoing(true);
+                    console.log("Event marked: attending successfully");
+                }
+            } else {
+                const response = await axios.delete(
+                    `http://localhost:5001/api/events/${id}/attend`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${localStorage.getItem(
+                                "userToken"
+                            )}`,
+                        },
+                    }
+                );
+                if (response.status === 200) {
+                    setIsGoing(false);
+                    console.log("Event marked: not attending successfully");
+                }
+            }
+        } catch (error) {
+            console.error("Error toggling attendance", error);
+        }
+    }
+
     return (
         <div className="flex px-9 mt-8 gap-5">
             <Button
-                onClick={() => console.log("I'm coming to this event!")}
+                onClick={handleGoingToggle}
                 variant="outline"
-                className="w-full rounded-3xl py-6 text-lg bg-white border-mint-500 border-2 hover:text-mint-500"
+                className={`w-full rounded-3xl py-6 text-lg ${
+                    isGoing
+                        ? `${
+                              isHovering
+                                  ? "bg-red-500 border-red-500 text-white hover:bg-red-600 hover:text-white"
+                                  : "bg-mint-500 border-mint-500 text-white"
+                          }`
+                        : "bg-white border-mint-500 hover:text-mint-500"
+                } border-2`}
+                onMouseEnter={() => setIsHovering(true)}
+                onMouseLeave={() => setIsHovering(false)}
             >
-                I'm In!
-                <IoHandLeft style={{ width: "20px", height: "20px" }} />
+                {isGoing && isHovering ? "I'm Out!" : "I'm In!"}
+                <IoHandLeft
+                    style={{
+                        width: "20px",
+                        height: "20px",
+                        color: isGoing ? "white" : "currentColor",
+                        display: isGoing && isHovering ? "none" : "block",
+                    }}
+                />
+                <CircleX
+                    style={{
+                        width: "20px",
+                        height: "20px",
+                        color: "white",
+                        display: isGoing && isHovering ? "block" : "none",
+                    }}
+                />
             </Button>
             <Button
                 onClick={() => console.log("I'm sharing this event!")}
