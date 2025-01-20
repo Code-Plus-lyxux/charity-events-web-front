339bbab47e409f3982b534640270287fadcebf43
Rasika Premachandra <130959620+rasikww@users.noreply.github.com>
2025-01-20 13:41:26 +0530

    add MediaViewDeletable component and add event image deleting function

--- src/components/event/EventTabsEditable.jsx
+++ src/components/event/EventTabsEditable.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import * as Tabs from "@radix-ui/react-tabs";
 import { CalendarDays, MapPin, ListFilter, Image } from "lucide-react";
 import FileUploader from "@/components/ui/FileUploader";
-import MediaViewer from "@/components/ui/MediaViewer";
+import MediaViewerDeletable from "@/components/ui/MediaViewerDeletable";
 
 export default function EventTabsEditable({ event }) {
     const [eventImages, setEventImages] = useState([]);
@@ -63,10 +63,11 @@ export default function EventTabsEditable({ event }) {
                     value="tab2"
                 >
                     <div className="mb-5 text-[15px] leading-normal">
-                        <MediaViewer
+                        <MediaViewerDeletable
                             eventImages={eventImages}
                             setEventImages={setEventImages}
-                        ></MediaViewer>
+                            event={event}
+                        ></MediaViewerDeletable>
                         <FileUploader
                             eventImages={eventImages}
                             event={event}
